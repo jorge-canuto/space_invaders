85661238030e5e9356bb61873e3c4ad41d63b531
Jorge Canuto <jluiz.481@gmail.com>
2022-04-01 15:21:42 -0300

    organizacao das imagens

--- constantes.rkt
+++ constantes.rkt
@@ -14,10 +14,9 @@
 (define ALTURA-CENARIO 150)
 (define LARGURA-CENARIO 200)
 (define CENARIO (empty-scene LARGURA-CENARIO ALTURA-CENARIO))
-(define IMG-NAVE (scale 0.3(bitmap "nave.png")))
-(define IMG-NAVE-INIMIGA (scale 0.5 (bitmap "nave_inimiga.png")))
-(define IMG-FUNDO-CENARIO (scale 1.9 (bitmap "fundo_cenario.png")))
-
+(define IMG-NAVE (scale 0.3(bitmap "C:/Users/Jorge/Downloads/UFPR/6° periodo/Oficina de Computação/desenvolvimento do jogo/space_invaders/images/nave.png")))
+(define IMG-NAVE-INIMIGA (scale 0.5 (bitmap "C:/Users/Jorge/Downloads/UFPR/6° periodo/Oficina de Computação/desenvolvimento do jogo/space_invaders/images/nave_inimiga.png")))
+(define IMG-FUNDO-CENARIO (scale 1.9 (bitmap "C:/Users/Jorge/Downloads/UFPR/6° periodo/Oficina de Computação/desenvolvimento do jogo/space_invaders/images/fundo_cenario.png")))
 (define LARGURA-NAVE (image-width IMG-NAVE))
 (define ALTURA-NAVE (image-height IMG-NAVE))
 (define LARGURA-NAVE-INIMIGA (image-width IMG-NAVE-INIMIGA))
